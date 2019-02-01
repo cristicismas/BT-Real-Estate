3375df5262c00b0d279fda103fc25587231d2d96
Cristi Cismas <cristicismas99@gmail.com>
2019-02-01 12:05:43 +0200

    Make listings rendering dynamic

--- btre/settings.py
+++ btre/settings.py
@@ -33,13 +33,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
-    'realtors.apps.RealtorsConfig',    
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
